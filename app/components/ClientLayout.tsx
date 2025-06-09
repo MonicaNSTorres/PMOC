1775d5d1a451ed4cdb0cd4fde04e3b86988ff23f
MonicaNSTorres <monicanstorress@gmail.com>
2025-06-09 09:27:19 -0300

    Corrigindo aparição da sidebar após login

--- app/components/ClientLayout.tsx
+++ app/components/ClientLayout.tsx
@@ -5,11 +5,15 @@ import SidebarPMOC from "./nav/navbar";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const auth = localStorage.getItem("pmoc_auth");
     setIsAuthenticated(auth === "true");
+    setLoading(false); //renderiza apos verificar
   }, []);
+
+  if (loading) return null; 
 
   return (
     <div className="flex">
