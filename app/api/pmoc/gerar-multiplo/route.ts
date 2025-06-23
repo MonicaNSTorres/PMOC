865954636882244a6420819fcb1437ab96605a07
MonicaNSTorres <monicanstorress@gmail.com>
2025-06-23 16:42:42 -0300

    Reestruturação para build

--- app/api/pmoc/gerar-multiplo/route.ts
+++ app/api/pmoc/gerar-multiplo/route.ts
@@ -1,5 +1,5 @@
 //import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import {prisma} from "@/lib/prisma";
 import { NextResponse } from "next/server";
 //import { prisma } from "@/lib/prisma";
 
